83195ad7a2d4eca2c52a1c0b0b72eb1407a7b1d8
Frine04 <angedayang237@gmail.com>
2025-06-08 08:39:52 +0200

    Delete convertisseur-de-devise-complet/src/main/resources directory

--- convertisseur-de-devise-complet/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO currency (code, name, rate_to_xof) VALUES 
-('XOF', 'Franc CFA', 1.0),
-('EUR', 'Euro', 655.957),
-('USD', 'Dollar Américain', 600.0),
-('GBP', 'Livre Sterling', 775.0),
-('NGN', 'Naira Nigérian', 1.37),
-('MAD', 'Dirham Marocain', 65.0);
